d0d4c391bbccbb3434e0349903f884f228cfb4c3
Piotr Bosianek <piotr.bosianek@gmail.com>
2023-07-04 22:52:56 +0200

    TUT-10 Make email necessary

--- app/core/models.py
+++ app/core/models.py
@@ -14,7 +14,9 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email: str, password=None, **extra_fields):
         """Create, save and return a new user."""
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('User must have an email address.')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
@@ -23,11 +25,13 @@ class UserManager(BaseUserManager):
 
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system"""
+
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
+    surname = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
 
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD = "email"
--- app/tests/test_models.py
+++ app/tests/test_models.py
@@ -13,13 +13,11 @@ def default_user() -> User:
     password = "test123qwerty"
     name = "Jan"
     surname = "Kowalski"
-    phone_number = "+48555555555"
     user = get_user_model().objects.create_user(
         email=email,
         password=password,
         name=name,
         surname=surname,
-        phone_number=phone_number,
     )
     return user
 
@@ -36,7 +34,6 @@ def test_create_default_user(default_user):
     assert_that(user.check_password(default_user.password))
     assert_that(user.name).is_equal_to(default_user.name)
     assert_that(user.surname).is_equal_to(default_user.surname)
-    assert_that(user.phone_number).is_equal_to(default_user.phone_number)
     assert_that(user.is_active).is_equal_to(True)
     assert_that(user.is_staff).is_equal_to(False)
-    assert_that(user.role).is_equal_to(User.Role.STUDENT)
+
