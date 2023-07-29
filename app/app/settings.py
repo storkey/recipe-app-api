dc4f847bd20cd36f60d2fff9f41f4e270656aea6
Piotr Bosianek <piotr.bosianek@gmail.com>
2023-07-29 21:11:33 +0200

    Change docker-compose.yaml and settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -89,6 +89,7 @@ DATABASES = {
         "NAME": os.environ.get("DB_NAME"),
         "USER": os.environ.get("DB_USER"),
         "PASSWORD": os.environ.get("DB_PASS"),
+        "PORT": 5433,
     }
 }
 
